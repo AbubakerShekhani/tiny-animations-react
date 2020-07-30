69ddd3f4555dbdf88e4b8a0165a9a0fe2bf26245
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-07-30 11:06:50 +0500

    Add Hero banner HTML and CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import introFrontLayer from './images/Intro_Front_Layer.svg';
+import introBrain from './images/Intro_Brain.svg';
+import featuredImageEmpty from './images/Intro_Featured_Image_Empty.svg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div className="main">
+        <div className="row row2 header">
+          <div className="row-inner">
+            <div id="anchor-top" className="header-container">
+
+              <div className="header-container-text">
+                <div className="header-container-description">
+                  <div className="header-container-title">
+                    <div className="header-container-title-2">CORTEX</div>
+                    <div className="header-container-title-3">COPYWRITER</div>
+                  </div>
+                  <div className="header-container-paragraph">
+                    <p className="intro-title">Enhance your communications with psychology-based copywriting and UX writing</p>
+                  </div>
+                  <div className="header-container-cta">Send a message</div>
+                </div>
+              </div>
+
+              <div className="header-container-image">
+                <img src={introFrontLayer} className="header-front" alt=" " />
+                <img src={featuredImageEmpty} alt=" " />
+                <img src={introBrain} className="header-brain" alt=" " />
+              </div>
+            </div>
+          </div>
+          <div className="header-container-bg"></div>
+        </div>
+      </div>
+
+    </>
   );
 }
 
